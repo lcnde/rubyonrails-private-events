9efa01945f8008a92fceb2d656eaa3bd8cb6fdaf
lcnde <176673026+lcnde@users.noreply.github.com>
2021-02-05 20:57:40 +0100

    events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,25 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :show]
+
+  def index
+    @event = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.new(event_params)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :date)
+  end
+
 end
