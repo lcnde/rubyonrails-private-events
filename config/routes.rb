823f24e0c3acb0479724c518f005613e612c5a9a
lcnde <176673026+lcnde@users.noreply.github.com>
2021-02-05 23:47:40 +0100

    custom route to use with the button in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "events#index"
   resources :events, only: [:index, :show, :new, :create]
+  
+  put '/event/:id/invited', to: 'inviteds#create', as: 'partecipate'
 
 end
