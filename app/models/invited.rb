89ca9fe2e5645c21fb2295ffd90a49d2ec8394a7
lcnde <176673026+lcnde@users.noreply.github.com>
2021-02-05 23:53:13 +0100

    user can sign up only once per event

--- app/models/invited.rb
+++ app/models/invited.rb
@@ -1,4 +1,5 @@
 class Invited < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  validates :user_id, uniqueness: {scope: :event_id} #makes sure a user cant sign up twice for the same event
 end
