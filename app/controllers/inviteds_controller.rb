7fb8470e66d9570db4990a9af6bd3405b8535ece
lcnde <176673026+lcnde@users.noreply.github.com>
2021-02-05 23:52:57 +0100

    fix events -> event

--- app/controllers/inviteds_controller.rb
+++ app/controllers/inviteds_controller.rb
@@ -1,2 +1,8 @@
 class InvitedsController < ApplicationController
+
+  def create
+    @event = Event.all.find(params[:id])
+    Invited.create(user_id: current_user.id, event_id: @event.id)
+    redirect_to event_path(@event)
+  end
 end
