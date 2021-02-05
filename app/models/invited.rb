1b4fb6f822e8060f41bdd10cc5be7aad2046b15b
lcnde <176673026+lcnde@users.noreply.github.com>
2021-02-05 20:40:57 +0100

    rails g invited

---
+++ app/models/invited.rb
@@ -0,0 +1,4 @@
+class Invited < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+end
---
+++ db/migrate/20210205192949_create_inviteds.rb
@@ -0,0 +1,10 @@
+class CreateInviteds < ActiveRecord::Migration[6.1]
+  def change
+    create_table :inviteds do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :event, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
