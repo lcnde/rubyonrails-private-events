af1cf38ef25591fe339c531e2336c0abe6888ee3
lcnde <176673026+lcnde@users.noreply.github.com>
2021-02-06 14:49:48 +0100

    added show route for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "events#index"
   resources :events, only: [:index, :show, :new, :create]
-  
+  resources :users, only: [:show]
+
   put '/event/:id/invited', to: 'inviteds#create', as: 'partecipate'
 
 end
