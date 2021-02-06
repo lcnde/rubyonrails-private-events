84d8e1aab885dc63c9c494129c1f253d35398653
lcnde <176673026+lcnde@users.noreply.github.com>
2021-02-06 14:28:39 +0100

    flash alert you're already subscribed

--- app/controllers/inviteds_controller.rb
+++ app/controllers/inviteds_controller.rb
@@ -2,12 +2,12 @@ class InvitedsController < ApplicationController
 
   def create
     @event = Event.all.find(params[:id])
-    Invited.create(user_id: current_user.id, event_id: @event.id)
-    redirect_to event_path(@event)
-    
-    if Invited.where(user_id: current_user.id, event_id: @event.id)#need to check if user is already invited to the event of the page youy're viewing
+    if Invited.where(user_id: current_user.id, event_id: @event.id).blank? == true#need to check if user is already invited to the event of the page youy're viewing
+      Invited.create(user_id: current_user.id, event_id: @event.id)
+      redirect_to event_path(@event)
+    else
       flash.alert = "You're already subscribed for the event."
-      #redirect_to event_path(@event)
+      redirect_to event_path(@event)
     end
   end
 end
